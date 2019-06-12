0a4dd9021402cd1399e2b01c82b8d846c5753271
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-06-12 13:23:27 +0530

    API fetch

--- JSON/ContentView.swift
+++ JSON/ContentView.swift
@@ -7,10 +7,48 @@
 //
 
 import SwiftUI
+import Combine
+
+struct Course: Decodable{
+    let name, imageURL: String
+}
+
+class NetworkManager: BindableObject {
+    var didChange = PassthroughSubject<NetworkManager,Never>()
+    
+    var courses = [Course]() {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    init() {
+        guard let url = URL(string: "http://api.letsbuildthatapp.com/jsondecodable/courses") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            guard let data = data else {return}
+            
+            let course = try! JSONDecoder().decode([Course].self, from: data)
+            
+            DispatchQueue.main.async {
+                self.courses = course
+            }
+            
+            print("Completed fetching")
+        }.resume()
+    }
+}
 
 struct ContentView : View {
+    
+    @State var networkManager = NetworkManager()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            List(networkManager.courses.identified(by: \.name)){
+                Text($0.name)
+                }.navigationBarTitle(Text("Courses"))
+        }
     }
 }
 
